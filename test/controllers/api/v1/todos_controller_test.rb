a333a7ae544a8ae0725836dc4b6df532f39573dc
David Perry <curiousepic@gmail.com>
2014-11-05 17:52:08 -0500

    Update and Delete actions and tests (one failing)

--- test/controllers/api/v1/todos_controller_test.rb
+++ test/controllers/api/v1/todos_controller_test.rb
@@ -13,8 +13,8 @@ class Api::V1::TodosControllerTest < ActionController::TestCase
       assert_not_nil assigns(:todos)
     end
     should "return all todos in DB" do
-      assert_equal Todo.count, json_response.count
-      json_response.each do |hash|
+      assert_equal Todo.count, json_response["todos"].count
+      json_response["todos"].each do |hash|
         todo = Todo.find(hash["id"])
         assert_equal hash["title"], todo.title
         assert_equal hash["order"], todo.order
@@ -24,15 +24,40 @@ class Api::V1::TodosControllerTest < ActionController::TestCase
 
   context "POST :create" do
     setup { post :create, {title: "new task", order: 1, completed: false } }
+    setup {@todo = assigns(:todo)}
     should respond_with(:created)
-    # should "save a new todo" do
-    #
-    # end
+    should "save a new todo" do
+      assert @todo.persisted?
+    end
     should "return the new todo" do
-      todo = assigns(:todo)
-      assert todo.persisted?
-      assert_not_nil todo.id
-      assert_equal todo.title, json_response["title"]
+      assert_not_nil @todo.id
+      assert_equal assigns(:title), json_response["title"]
+    end
+  end
+
+  context "PUT :update" do
+    setup do
+      @todo = todos(:one)
+      put :update, {id: @todo.id, title: "New title"}
+    end
+    should respond_with(:ok)
+    should "update todo" do
+      @todo.reload
+      assert_equal "New title", @todo.title
+    end
+    should "return updated todo" do
+      assert_equal "New title", json_response["title"]
+    end
+  end
+
+  context "DELETE :destroy" do
+    setup do
+      @todo = todos(:one)
+    end
+    should "delete todo" do
+      assert_difference('Todo.count', -1) do
+        delete :destroy, {id: @todo.id}
+      end
     end
   end
 end
